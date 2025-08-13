68fdc9b6832b977a365b29dece772eae09d59a13
xietianxiang <1228303666@qq.com>
2025-08-13 14:30:56 +0800

    test

--- src/test.cpp
+++ src/test.cpp
@@ -5,24 +5,24 @@
 #ifndef WX_PRECOMP
 #include <wx/wx.h>
 #endif
-#include "S57viewr.h"
+// #include "S57viewr.h"
 
-class S57App : public wxApp
-{
-public:
-    virtual bool OnInit();
-};
+// class S57App : public wxApp
+// {
+// public:
+//     virtual bool OnInit();
+// };
 
-wxIMPLEMENT_APP(S57App);
+// wxIMPLEMENT_APP(S57App);
 
-bool S57App::OnInit()
-{
-    if (!wxApp::OnInit())
-        return false;
+// bool S57App::OnInit()
+// {
+//     if (!wxApp::OnInit())
+//         return false;
 
-    // 创建主窗口
-    S57Viewer* frame = new S57Viewer("S-57 Chart Viewer", wxPoint(50, 50), wxSize(1024, 768));
-    frame->Show(true);
+//     // 创建主窗口
+//     S57Viewer* frame = new S57Viewer("S-57 Chart Viewer", wxPoint(50, 50), wxSize(1024, 768));
+//     frame->Show(true);
     
-    return true;
-}
+//     return true;
+// }
